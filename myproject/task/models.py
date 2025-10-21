290927489a54600586a06cd23a44c3dfc3959b10
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-10-21 21:10:28 +0800

    fix models

--- myproject/task/models.py
+++ myproject/task/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     class Meta:
         abstract = True
